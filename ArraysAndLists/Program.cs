155f10e23c991a6da5d95092d4719b6a7fee5e00
Cameron Graybill <cdgraybill@gmail.com>
2020-01-22 08:35:37 -0600

    finished assignment

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -26,6 +26,7 @@ namespace ArraysAndLists
              * or the odds List
              */
 
+            Console.WriteLine("Odds");
             for (int i = 1; i < 10; i += 2)
             {
                 odds.Add(i);
@@ -36,10 +37,12 @@ namespace ArraysAndLists
                 Console.WriteLine(num1);
             }
 
+            Console.WriteLine();
 
-            for (int i = 2; i < 10; i += 2)
+            Console.WriteLine("Evens");
+            for (int i = 2; i < 11; i += 2)
             {
-                odds.Add(i);
+                evens.Add(i);
             }
 
             foreach (var num2 in evens)
@@ -47,6 +50,9 @@ namespace ArraysAndLists
                 Console.WriteLine(num2);
             }
 
+            Console.WriteLine();
+            Console.WriteLine("Press any key.");
+            Console.ReadLine();
             /* Now using foeach or for loops,
              * display each List of even and odd numbers
              *
